be246e50c0b1bb5ad013f2d6db6e63b32e5adaad
angel <rafaelprojmgz@gmail.com>
2025-12-03 10:06:28 -0400

    feat:

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ease-point.onrender.com/",
+  baseURL: "https://ease-point.onrender.com",
 });
 
 api.interceptors.request.use(
