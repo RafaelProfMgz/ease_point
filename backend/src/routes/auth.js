050260fe85b24005976b407674c43c22ea3a2d64
RafaelProfMgz <rafaelprojmgz@gmail.com>
2025-12-04 19:02:21 -0400

    fet

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.post("/logout", controller.logout);
+
+// --- ROTAS PROTEGIDAS ---
+router.get("/me", authMiddleware, controller.getMe);
 
 module.exports = router;
