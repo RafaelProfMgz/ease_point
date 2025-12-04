c628d693ae83ea719166872bb09453ddc6c5878f
RafaelProfMgz <rafaelprojmgz@gmail.com>
2025-12-04 18:29:55 -0400

    feat

--- backend/src/models/providerModel.js
+++ backend/src/models/providerModel.js
@@ -4,6 +4,8 @@ class ProviderModel {
   static async getOAuthUrl(provider) {
     const redirectBase = process.env.FRONTEND_URL || "http://localhost:3000";
 
+    console.log("Redirect Base sendo usada:", redirectBase);
+
     const { data, error } = await supabaseAdmin.auth.signInWithOAuth({
       provider: provider,
       options: {
